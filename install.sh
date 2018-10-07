a366b11e5619714da3c19d61648ff1e375589874
Kareem Moussa <kareem.moussa@coinbase.com>
2018-10-07 16:27:11 -0700

    npm installs in install script

--- install.sh
+++ install.sh
@@ -2,5 +2,9 @@
 
 install_0xd3bu6() {
   git clone https://github.com/itstehkman/ethereumjs-vm
+  npm install
   git clone https://github.com/itstehkman/ganache-core
+  npm install
+  # Important to link to local EVM package for my changes
+  npm link ../ethereumjs-vm
 }
